31a7a4cf99ee0c765673cd8712b981d3b56bcaff
Mia-Zada <miazada1@gmail.com>
2025-11-06 20:02:15 +0200

    Update query_3.sql

--- sql/query_3.sql
+++ sql/query_3.sql
@@ -16,7 +16,7 @@ with corr_doses_death as
 	order by cor_death_doses 
 )
 select  count (*) as countries, 
-concat(round( 100*count (case when cor_after21 < -0.5 then 1 end  )/ count (*),1 ), '%') as "after_21_between_(-1)_(-0.5)", 
-concat (round( 100* count ( case when cor_after21 between -0.5 and 0.5 then 1 end )/ count (*), 1), '%') as "after_21_between_(-0.5)_(0.5)", 
-concat (round ( 100* count ( case when cor_after21> 0.5 then 1 end)/ count (*), 1) , '%') as "after_21_between_(0.5)_(1)" 
-from corr_doses_death_results
+concat(round ( 100 * sum (case when cor_death_doses < -0.5 then 1 else 0 end  )/ count (*), 1), '%')  as "between_(-1)_(-0.5)", 
+concat(round( 100 * sum ( case when cor_death_doses between -0.5 and 0.5 then 1 else 0 end )/ count (*), 1),  '%') as "between_(-0.5)_(0.5)", 
+concat(round( 100* sum ( case when cor_death_doses >0.5 then 1 else 0 end)/count (*), 1), '%') as "between_(0.5)_(1)"
+from corr_doses_death
